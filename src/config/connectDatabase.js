1a57664bd6306428ed0f8db95fe99d4e4878dd5b
hiep <hiepdev.js@gmail.com>
2023-05-09 22:19:23 +0700

    deploy: change connect db

--- src/config/connectDatabase.js
+++ src/config/connectDatabase.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require("sequelize");
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("phontro123", "root", null, {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false
+const sequelize = new Sequelize("db_phongtro123_jrcv", "db_phongtro123_jrcv_user", "RtzIjZ6sMaLb4I9bhtGbAKhAMeEWR9BW", {
+  host: "dpg-chd2qim7avjcvo54d0u0-a.singapore-postgres.render.com",
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 const connectDb = async () => {
