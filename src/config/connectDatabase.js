661322b125bc93fbbf6a9fb46e6dee84ee50d71e
hiep <hiepdev.js@gmail.com>
2023-05-11 03:24:02 +0700

    build: check ssl

--- src/config/connectDatabase.js
+++ src/config/connectDatabase.js
@@ -1,10 +1,18 @@
 const { Sequelize } = require("sequelize");
+const environment = process.env.NODE_ENV;
 
 // Option 3: Passing parameters separately (other dialects)
+console.log(environment)
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD || null, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
   logging: false,
+  dialectOptions: environment === "production" ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  } : null
 });
 
 const connectDb = async () => {
